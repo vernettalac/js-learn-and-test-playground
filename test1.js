433aaa5d19fd97560e3d00e8d17280622b4b613a
Robert <122917605+vernettalac@users.noreply.github.com>
2023-12-06 17:16:20 +0300

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>DOM Manipulation</title>
+</head>
+<body>
+     <h1 id="demo">Hello, JavaScript!</h1>
+     <script>
+         // Change text using JavaScript
+         document.getElementById("demo").innerHTML = "Hello, DOM!";
+     </script>
+</body>
+</html>
